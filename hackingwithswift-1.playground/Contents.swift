5c0d086258c506ce3af61c5f84dbfbe3a69398b3
Yasin Aktas <normyrhym@gmail.com>
2023-08-19 16:40:34 +0300

    Hacking with Swift Day 2

---
+++ hackingwithswift-1.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let temperatureCelcius = 25.0
+let temperatureFahrenheit = (temperatureCelcius * 9) / 5 + 32
+let output = "\(temperatureCelcius) Celcius equals \(temperatureFahrenheit) Fahrenheits"
+print(output)
