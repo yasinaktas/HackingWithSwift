2480cfb852694f5f2dc3b9792694212b70bcad29
Yasin Aktas <normyrhym@gmail.com>
2023-08-20 16:59:21 +0300

    Hacking with Swift Checkpoint 2

---
+++ hackingwithswift-2.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var names = ["yasin","celal","ömer","umut","yasin","umut"]
+var namesSet = Set(names)
+
+print("There are \(names.count) names and there are \(namesSet.count) unique names in the list")
